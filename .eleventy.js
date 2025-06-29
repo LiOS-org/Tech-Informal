19f878c879b521b0d9a12b4aeac018c4492978a9
MD Saifullah <muhammadsaifullah2223@gmail.com>
2025-06-29 20:22:30 +0530

    added featured blogs in individula blogs

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 const { DateTime } = require("luxon");
-module.exports = function(eleventyConfig) {
 
-    // Add a passthrough copy for the CSS file
+module.exports = function(eleventyConfig) {
+    // Passthrough copies
     eleventyConfig.addPassthroughCopy("./src/assets");
     eleventyConfig.addPassthroughCopy("./src/assets/firebase.js");
     eleventyConfig.addPassthroughCopy("./src/assets/css");
@@ -11,14 +11,29 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/admin");
     eleventyConfig.addPassthroughCopy("./src/404.html");
     eleventyConfig.addPassthroughCopy("./src/onesignal.js");
+
+    // Filters
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString("DateTime.DATE_MED");
-    }
-    );
+    });
+
+    eleventyConfig.addFilter("shuffle", function(array) {
+        let shuffled = [...array];
+        for (let i = shuffled.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+        }
+        return shuffled;
+    });
+
+    eleventyConfig.addFilter("limit", function(array, count) {
+        return array.slice(0, count);
+    });
+
     return {
         dir: {
             input: "src",
             output: "public",
         }
-    }
+    };
 };
